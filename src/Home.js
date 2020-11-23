fff57a6e96be0d5e4b164b69adbc5ba29901a7ab
amadou-20 <ahmo300@outlook.com>
2020-11-23 22:56:10 +0100

    the airbnb-clone2 first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+
+
+    return (
+            <div className="home">
+               <h1>this the home component</h1> 
+            </div>
+        );
+}
+
+export default Home
+
+
