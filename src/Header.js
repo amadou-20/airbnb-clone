91e21d92e24d4deac33e9d1068127f219106eb2b
amadou-20 <ahmo300@outlook.com>
2020-11-29 23:01:40 +0100

    Adding the banner.jd file and banner.css here

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Header.css";
+
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import "./Home.css";
+import Banner from "./Banner";
+
+
 
 function Home() {
 
 
     return (
             <div className="home">
-               <h1>this the home component</h1> 
+               <h1>home component</h1>
+               <Banner />
             </div>
         );
 }
 
-export default Home
+export default Home;
 
 
